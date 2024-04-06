1fbdc84e29c86b164260b46da3ef9979e45506de
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-06 14:13:48 +0545

    feat: auto generated migration

---
+++ src/Migration/Version20240331101339.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HRHub\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240331101339 extends AbstractMigration {
+
+	public function getDescription(): string {
+		return '';
+	}
+
+	public function up( Schema $schema ): void {
+		// this up() migration is auto-generated, please modify it to your needs
+	}
+
+	public function down( Schema $schema ): void {
+		// this down() migration is auto-generated, please modify it to your needs
+	}
+}
