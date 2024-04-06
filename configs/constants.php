515069b7c9dc0bb9223a770a233ca5ec239072c2
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-06 14:12:06 +0545

    feat: add constants config

---
+++ configs/constants.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Constants.
+ */
+
+define( 'HRHUB_PLUGIN_FILE', dirname( __DIR__ ) . '/app.php' );
+define( 'HRHUB_PLUGIN_DIR', plugin_dir_path( HRHUB_PLUGIN_FILE ) );
+define( 'HRHUB_VERSION', '0.1.0' );
