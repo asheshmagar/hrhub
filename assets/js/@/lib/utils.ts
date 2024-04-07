89e9eb15a4ff19a7501057f3c9f13f5e06021fb6
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-07 21:45:24 +0545

    feat: add helper functions

--- assets/js/@/lib/utils.ts
+++ assets/js/@/lib/utils.ts
@@ -8,3 +8,20 @@ export function cn(...inputs: ClassValue[]) {
 export function trailingslashit(url: string) {
 	return url.endsWith('/') ? url : url + '/';
 }
+
+export const file2blob = (file: File) => {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsArrayBuffer(file);
+		reader.onload = () => resolve(reader.result);
+		reader.onerror = (error) => reject(error);
+	});
+};
+
+export const files2formData = (files: File[]) => {
+	const formData = new FormData();
+	for (let i = 0; i < files.length; i++) {
+		formData.append(`documents.${i}`, files[i]);
+	}
+	return formData;
+};
