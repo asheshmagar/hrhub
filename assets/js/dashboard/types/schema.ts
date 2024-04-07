147cfbb315b6014f1e7e1632510caac9100b180e
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-07 21:49:21 +0545

    add: schemas

--- assets/js/dashboard/types/schema.ts
+++ assets/js/dashboard/types/schema.ts
@@ -1,5 +1,79 @@
 import { z } from 'zod';
 
+const MAX_FILE_SIZE = 1024 * 1024 * 2;
+
+const ACCEPTED_FILE_TYPES = ['jpeg', 'jpg', 'png', 'webp', 'pdf'];
+
+export const attachmentSchema = z.object({
+	id: z.number(),
+	title: z.string(),
+	filename: z.string(),
+	url: z.string().url(),
+	link: z.string().url(),
+	alt: z.string().optional(),
+	author: z.string(), // Assuming author is always a string
+	description: z.string().optional(),
+	caption: z.string().optional(),
+	name: z.string(),
+	status: z.string(),
+	uploadedTo: z.number(),
+	date: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/),
+	modified: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/),
+	menuOrder: z.number(),
+	mime: z.string(),
+	type: z.string(),
+	subtype: z.string(),
+	icon: z.string().url(),
+	dateFormatted: z.string(),
+	nonces: z.object({
+		update: z.string(),
+		delete: z.string(),
+		edit: z.string(),
+	}),
+	editLink: z.string().url(),
+	meta: z.boolean(),
+	authorName: z.string(),
+	authorLink: z.string().url(),
+	filesizeInBytes: z.number(),
+	filesizeHumanReadable: z.string(),
+	context: z.string().optional(),
+	height: z.number(),
+	width: z.number(),
+	orientation: z.string(),
+	sizes: z.object({
+		thumbnail: z.object({
+			height: z.number(),
+			width: z.number(),
+			url: z.string().url(),
+			orientation: z.string(),
+		}),
+		medium: z.object({
+			height: z.number(),
+			width: z.number(),
+			url: z.string().url(),
+			orientation: z.string(),
+		}),
+		large: z.object({
+			height: z.number(),
+			width: z.number(),
+			url: z.string().url(),
+			orientation: z.string(),
+		}),
+		full: z.object({
+			url: z.string().url(),
+			height: z.number(),
+			width: z.number(),
+			orientation: z.string(),
+		}),
+	}),
+	compat: z.object({
+		item: z.string(),
+		meta: z.string(),
+	}),
+});
+
+export type AttachmentSchema = z.infer<typeof attachmentSchema>;
+
 export const employeeSchema = z.object({
 	name: z.string().min(1, 'Name is required').max(255),
 	email: z.string().min(1, 'Email is required').email('Email is not valid'),
@@ -10,10 +84,14 @@ export const employeeSchema = z.object({
 	address: z.string().min(1).max(255),
 	date_of_birth: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
 	date_of_employment: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
-	salary: z.number().min(0),
+	salary: z.number().min(0).optional(),
 	status: z.enum(['active', 'inactive', 'terminated']).default('inactive'),
 	department: z.number().optional(),
 	position: z.number().optional(),
+	employment_type: z
+		.enum(['full-time', 'part-time', 'trainee/intern', 'contractor/freelancer'])
+		.default('full-time'),
+	documents: z.array(attachmentSchema).optional(),
 });
 
 export type EmployeeSchema = z.infer<typeof employeeSchema>;
