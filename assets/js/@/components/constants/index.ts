86eb3d05923e5661949884c6b72684825fd67070
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-13 21:52:42 +0545

    feat: nav items roles

--- assets/js/@/components/constants/index.ts
+++ assets/js/@/components/constants/index.ts
@@ -6,35 +6,47 @@ export const navItems: NavItem[] = [
 		href: '/',
 		icon: 'dashboard',
 		label: 'Dashboard',
+		roles: ['administrator', 'hrhub_manager', 'hrhub_employee'],
 	},
 	{
 		title: 'Employees',
 		href: '/employees',
 		icon: 'employee',
 		label: 'Employees',
+		roles: ['administrator', 'hrhub_manager'],
 	},
 	{
 		title: 'Departments',
 		href: '/departments',
 		icon: 'kanban',
 		label: 'Departments',
+		roles: ['administrator', 'hrhub_manager'],
 	},
 	{
 		title: 'Positions',
 		href: '/positions',
 		icon: 'laptop',
 		label: 'Positions',
+		roles: ['administrator', 'hrhub_manager'],
 	},
 	{
 		title: 'Leaves',
 		href: '/leaves',
 		icon: 'arrowRight',
 		label: 'Leaves',
+		roles: ['administrator', 'hrhub_manager', 'hrhub_employee'],
 	},
 	{
 		title: 'Reviews',
 		href: '/reviews',
 		icon: 'stars',
 		label: 'Reviews',
+		roles: ['administrator', 'hrhub_manager', 'hrhub_employee'],
 	},
+];
+
+export const ALLOWED_ROLES = [
+	'administrator',
+	'hrhub_manager',
+	'hrhub_employee',
 ];
