14a73c7322a62c91fed657f7149a3ed4764018e4
asheshmagar <asheshmagar8888667@gmail.com>
2024-04-13 21:47:32 +0545

    feat: init hooks and version checks and load text domain

--- src/App.php
+++ src/App.php
@@ -5,6 +5,7 @@
 
 namespace HRHub;
 
+use HRHub\Email\EmailHooks;
 use HRHub\Traits\Hook;
 
 /**
@@ -29,6 +30,7 @@ class App {
 		hrhub( ScriptStyle::class )->init();
 		hrhub( Admin::class )->init();
 		hrhub( RESTApi::class )->init();
+		hrhub( EmailHooks::class )->init();
 		$this->add_action( 'init', [ $this, 'after_wp_init' ], 0 );
 	}
 
@@ -38,6 +40,34 @@ class App {
 	 * @return void
 	 */
 	public function after_wp_init() {
+		$this->version_check();
+		$this->load_textdomain();
 		$this->action( 'init' );
+	}
+
+	/**
+	 * Version check.
+	 *
+	 * @return void
+	 */
+	private function version_check() {
+		$current_version = get_option( '_hrhub_version' );
+		if ( empty( $current_version ) ) {
+			update_option( '_hrhub_version', HRHUB_VERSION );
+			return;
+		}
+		if ( version_compare( $current_version, HRHUB_VERSION, '<' ) ) {
+			$this->action( 'version:update', HRHUB_VERSION, $current_version );
+			update_option( '_hrhub_version', HRHUB_VERSION );
+		}
+	}
+
+	/**
+	 * Load text domain.
+	 *
+	 * @return void
+	 */
+	private function load_textdomain() {
+		load_plugin_textdomain( 'hrhub', false, HRHUB_PLUGIN_DIR . '/translations' );
 	}
 }
