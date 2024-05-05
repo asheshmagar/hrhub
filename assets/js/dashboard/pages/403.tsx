b442cb52a07fbccf3ee22a68fa2fae6a7f04a038
asheshmagar <asheshmagar8888667@gmail.com>
2024-05-05 09:56:20 +0545

    add: 403 access forbidden component

---
+++ assets/js/dashboard/pages/403.tsx
@@ -0,0 +1,14 @@
+import { __ } from '@wordpress/i18n';
+import React from 'react';
+export const NoPermission = () => {
+	return (
+		<div className="h-full w-full flex flex-col justify-center items-center mt-20">
+			<h1 className="text-9xl font-extrabold text-black tracking-widest">
+				403
+			</h1>
+			<div className="bg-gray-300 px-2 text-sm rounded rotate-12 absolute uppercase">
+				{__('Not this time, access forbidden', 'hrhub')}
+			</div>
+		</div>
+	);
+};
