365f7bbd4e3ec9398cae746b43c69f182240c39e
asheshmagar <asheshmagar8888667@gmail.com>
2024-05-30 14:45:08 +0545

    feat: translatable static texts

--- assets/js/dashboard/pages/404.tsx
+++ assets/js/dashboard/pages/404.tsx
@@ -1,13 +1,14 @@
+import { __ } from '@wordpress/i18n';
 import React from 'react';
 
 export const NotFound = () => {
 	return (
 		<div className="h-full w-full flex flex-col justify-center items-center mt-20">
 			<h1 className="text-9xl font-extrabold text-black tracking-widest">
-				404
+				{__('404', 'hrhub')}
 			</h1>
 			<div className="bg-gray-300 px-2 text-sm rounded rotate-12 absolute">
-				Page Not Found
+				{__('Page Not Found', 'hrhub')}
 			</div>
 		</div>
 	);
