222f14bdd6464441222318709b9c266f97bd6546
asheshmagar <asheshmagar8888667@gmail.com>
2024-05-30 14:45:32 +0545

    feat: fix translation

--- assets/js/dashboard/pages/403.tsx
+++ assets/js/dashboard/pages/403.tsx
@@ -4,7 +4,7 @@ export const NoPermission = () => {
 	return (
 		<div className="h-full w-full flex flex-col justify-center items-center mt-20">
 			<h1 className="text-9xl font-extrabold text-black tracking-widest">
-				403
+				{__('403', 'hrhub')}
 			</h1>
 			<div className="bg-gray-300 px-2 text-sm rounded rotate-12 absolute uppercase">
 				{__('Not this time, access forbidden', 'hrhub')}
